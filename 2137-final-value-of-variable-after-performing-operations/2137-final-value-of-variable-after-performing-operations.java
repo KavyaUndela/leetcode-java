67d36baa085f5e33592528d99d0948bdb37ec24a
Undela Kavya Saranya <181745091+KavyaUndela@users.noreply.github.com>
2025-07-27 17:33:26 +0530

    Time: 1 ms (78.08%), Space: 42.9 MB (30.29%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -5,7 +5,7 @@ class Solution {
             if(operations[i].equals("++X") ||operations[i].equals("X++")){
                 ans= ans+1;
             }
-            if(operations[i].equals("X--") || operations[i].equals("--X")){
+            else{
               ans= ans-1;
             }
         }
